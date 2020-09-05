dd1b36669bff88acd5d980bbc3ca870d5c1c4d11
nickihashemi <nhashemi@dons.usfca.edu>
2020-09-04 18:55:46 -0700

    needed: comments

--- src/com/company/TwoSum.java
+++ src/com/company/TwoSum.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class TwoSum {
 
+    //TODO: twoSum header ok?
+    //TODO: user input array?
     //algorithm
     //1. user inputs an array
     //2. selection sort
@@ -72,26 +74,12 @@ public class TwoSum {
 
     }
 
-    public static void main(String[] args) {
-
-        TwoSum sorter = new TwoSum();
-        int[] arr = {7, 5, 3, 3, 2, 1, 4, 6, 12, 17};
-        int sum = 6;
-
-        sorter.sort(arr);
-
-        System.out.print("Array: [");
-        for (int i=0; i< arr.length-1; i++) {
-            System.out.print(arr[i] + ", ");
-        }
-        System.out.println(arr[arr.length-1] + "]");
-
-
+    public int[] twoSum(int[] num, int sum) {
         int check = 0;
-        for (int i=0; i<arr.length-1; i++) {
-            for (int j=i+1; j<arr.length; j++) {
-                if (arr[i] != arr[j]) {
-                    if (arr[i] + arr[j] == sum) {
+        for (int i=0; i<num.length-1; i++) {
+            for (int j=i+1; j<num.length; j++) {
+                if (num[i] != num[j]) {
+                    if (num[i] + num[j] == sum) {
                         System.out.print("Possible Sum: ");
                         System.out.println("[" + i + ", " + j + "]");
                         check = 1;
@@ -102,7 +90,27 @@ public class TwoSum {
         }
 
         if(check == 0) {
-            System.out.println("-1");
+            System.out.println("Possible Sum: -1 (Not found)");
         }
+        return num;
+    }
+
+    public static void main(String[] args) {
+
+        TwoSum sorter = new TwoSum();
+        int[] num = {7, 5, 3, 3, 2, 1, 4, 6, 12, 17};
+        int sum = 50;
+
+        sorter.sort(num);
+
+        System.out.print("Array: [");
+        for (int i=0; i< num.length-1; i++) {
+            System.out.print(num[i] + ", ");
+        }
+        System.out.println(num[num.length-1] + "]");
+
+        sorter.twoSum(num, sum);
+
+
     }
 }
